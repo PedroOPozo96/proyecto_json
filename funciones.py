a148c6335bde864cd4d6cff26730c4dafa6e745a
PedroOPozo96 <168337714+PedroOPozo96@users.noreply.github.com>
2024-06-20 11:29:14 +0200

    Commit

--- funciones.py
+++ funciones.py
@@ -1,41 +1,107 @@
-   
+import json
+
+#Datos del Json
+
+def cargar_json():
+    archivo = 'libros.json'
+    with open(archivo, 'r', encoding = 'utf-8') as fichero:
+        datos = json.load(fichero)
+    return datos['libros']
 
 
-# 1. Lista de los títulos de los libros.
+#Menú
 
-def listar_libros(libros):
-    titulos = [libro['nombre'] for libro in libros]
-    return titulos
 
-#2. Contar el número total de libros.
+def mostrar_menu():
+    print("\n Menú: ")
+    print("1. Lista de los libros.")
+    print("2. Contar el número total de libros. ")
+    print("3. Libros por año de publicación.")
+    print("4. Libros por autor.")
+    print("5. Libros por precio.")
+    print("6. Salir del programa.")
+
+
+
+
+
+#1. Lista de los libros.
+
+
+def lista_libros(libros):
+    libros_por_pagina = 10
+    total_libros = len(libros)
+    paginas = (total_libros + libros_por_pagina - 1) // libros_por_pagina
+    pagina_actual = 0
+    continuar = True
+
+    
+    while continuar and pagina_actual < paginas:
+        print(f"\nPágina {pagina_actual + 1} de {paginas}")
+        inicio = pagina_actual * libros_por_pagina
+        fin = inicio + libros_por_pagina
+
+        for libro in libros[inicio:fin]:
+              print(f"• \033[1m{libro['nombre']}\033[0m")
+              print("")
+
+        if pagina_actual + 1 < paginas:
+            opcion = input("Indica 's' para siguiente página o 'm' para volver al menú: ")
+            if opcion.lower() == 's':
+                    pagina_actual += 1
+            elif opcion.lower() =='m':
+                    continuar = False
+            else: 
+                    print ('Opción no válida.')
+        else:
+             input("Presione 'm' para volver")
+             continuar = False
+
+                
+
+#2. Contar el número de libros
 
 def contar_libros(libros):
     return len(libros)
 
 
-#3.Buscar libro por fecha de publicación
+#3. Buscar libro por fecha de publicación
 
 def libros_por_fecha(libros, fecha):
-     libros_fecha = [libro for libro in libros if libro['fecha_publicacion'] == str(fecha)]
-     return libros_fecha
+    libros_fecha = [libro for libro in libros if libro['fecha_publicacion'] == str(fecha)]
+    print(f"\n Libros publicados en {fecha}: ")
+    for libro in libros_fecha:
+        print(f"• \033[1m{libro['nombre']}\033[0m")
+        print("")
 
 
-#4. Buscar el nombre del autor y que te devuelva el libro.
+
+
+
+#4. Buscar libro por autor
 
 def titulos_por_autor(libros, autor):
-    titulos_autor = [libro['nombre'] for libro in libros if libro['autor'] == autor]
-    return titulos_autor
+    libros_autor = [libro for libro in libros if libro['autor'] == autor]
+    print(f"\nTítulos de libros escritos por {autor}:")
+    for libro in libros_autor:
+        print(f"• \033[1m{libro['nombre']}\033[0m")
+        print("") 
 
 
 
 
-#5. Buscar libros por Precio.
+#5. Libros por precio
 
-def buscar_libros_por_precio(libros, precio):  
+def libros_por_precio(libros, precio):
     libros_precio = [libro for libro in libros if libro['precio'] == precio]
-    return libros_precio
+    if libros_precio:
+                for libro in libros_precio:
+                    
+                    print(f"Título: \033[1m{libro['nombre']}\033[0m")
+                    print(f"Autor: \033[1m{libro['autor']}\033[0m")
+                    print(f"Año de Publicación: \033[1m{libro['fecha_publicacion']}\033[0m")
+                    print(f"Editorial: \033[1m{libro['editorial']}\033[0m")
+                    print("-" * 40)
 
-
-
-
-
+    else:
+        print("No está disponible el libro con ese precio")
--- programa.py
+++ programa.py
@@ -1,78 +1,45 @@
-import json
-from funciones  import listar_libros, contar_libros, libros_por_fecha, titulos_por_autor, buscar_libros_por_precio
+from funciones import (cargar_json, mostrar_menu, lista_libros, contar_libros, libros_por_fecha, libros_por_precio, titulos_por_autor)
 
-def cargar_datos():
-    archivo = 'libros.json'
-    with open(archivo, 'r', encoding='utf-8') as fichero:
-        datos = json.load(fichero)
-    return datos['libros']
 
-def mostrar_menu():
-    print("\nMenú de Opciones")
-    print("1. Listar los títulos de todos los libros")
-    print("2. Contar el número total de libros")
-    print("3. Buscar libros por año de publicación")
-    print("4. Mostrar títulos de libros por un autor específico")
-    print ("5. Buscar libros por el precio")
-    print("6. Salir")
+#Progrma Principal
 
 
 
 def main():
+    libros = cargar_json()
 
-    libros = cargar_datos()
-    
     programa_principal = True
     while programa_principal:
         mostrar_menu()
-        opcion = input("Seleccione una opción: ")
+        opcion = input("Selecciona una opción: ")
+
 
         if opcion == '1':
-            print("\nLista de todos los libros:")
-            for titulo in listar_libros(libros):
-                print(titulo)
+            lista_libros(libros)
 
         elif opcion == '2':
-            print("\nNúmero total de libros:")
+            print("\n Nº de Libros: ")
             print(contar_libros(libros))
-
-        elif opcion == '3':
-            fecha = input("Introduce el año de publicación: ")
-            print(f"\nLibros publicados en {fecha}:")
-            for libro in libros_por_fecha(libros, fecha):
-                print(libro['nombre'])
-
-        elif opcion == '4':
-            autor = input("Introduce el nombre del autor: ")
-            print(f"\nTítulos de libros escritos por {autor}:")
-            for titulo in titulos_por_autor(libros, autor):
-                print(titulo)
-
         
+        elif opcion == '3':
+            fecha = input("Indica el año de publicación: ")
+            libros_por_fecha(libros, fecha)
+        
+        elif opcion == '4':
+            autor = input("Indica el autor del libro: ")
+            titulos_por_autor(libros, autor)
+
+
         elif opcion == '5':
-            precio = float(input("Introduce el precio del libro: "))
-            print(f"\nLibros con ese precio {precio}:")
-            libros_precio = buscar_libros_por_precio(libros, precio)  
-            if libros_precio:
-                for libro in libros_precio:
-                    
-                    print(f"Título: {libro['nombre']}")
-                    print(f"Autor: {libro['autor']}")
-                    print(f"Año de Publicación: {libro['fecha_publicacion']}")
-                    print(f"Editorial: {libro['editorial']}")
-                    print("-" * 40)
+            precio = float(input("Indica el precio del libro: "))
+            libros_por_precio(libros, precio)
 
-
-            else:
-                print("No está disponible el libro con ese precio")
-
-                
         elif opcion == '6':
-                    # Salir
-                    programa_principal = False
+            programa_principal = False
 
         else:
-            print("Opción no válida. Por favor, seleccione una opción válida.")
+            print("Opción erronea, selecciona una correcta.")
+
 
 if __name__ == "__main__":
     main()
